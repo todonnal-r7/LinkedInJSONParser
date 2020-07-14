f771fe783a77ab9414510d86f2519ab1b5a10604
todonnal-r7 <31863079+todonnal-r7@users.noreply.github.com>
2020-07-14 09:11:13 -0500

    Add files via upload

--- LinkedInJSONParser.py
+++ LinkedInJSONParser.py
@@ -10,9 +10,17 @@ inputFile = args.file
 with open(inputFile) as f:
 	jsonData = json.load(f)
 
-for x in range(0,len(jsonData['data']['elements'][0]['elements'])):
-	fullName = jsonData['data']['elements'][0]['elements'][x]['title']['text']
-	if (fullName == 'LinkedIn Member' or fullName == ''):
-		continue
-	else:
-		print(jsonData['data']['elements'][0]['elements'][x]['title']['text']+":"+jsonData['data']['elements'][0]['elements'][x]['headline']['text'])+":"+jsonData['data']['elements'][0]['elements'][x]['subline']['text']
+if (len(jsonData['data']['elements']) > 1):
+	for x in range(0,len(jsonData['data']['elements'][1]['elements'])):
+		fullName = jsonData['data']['elements'][1]['elements'][x]['title']['text']
+		if (fullName == 'LinkedIn Member' or fullName == ''):
+			continue
+		else:
+			print(jsonData['data']['elements'][1]['elements'][x]['title']['text']+":"+jsonData['data']['elements'][1]['elements'][x]['headline']['text'])+":"+jsonData['data']['elements'][1]['elements'][x]['subline']['text']
+else:
+	for x in range(0,len(jsonData['data']['elements'][0]['elements'])):
+		fullName = jsonData['data']['elements'][0]['elements'][x]['title']['text']
+		if (fullName == 'LinkedIn Member' or fullName == ''):
+			continue
+		else:
+			print(jsonData['data']['elements'][0]['elements'][x]['title']['text']+":"+jsonData['data']['elements'][0]['elements'][x]['headline']['text'])+":"+jsonData['data']['elements'][0]['elements'][x]['subline']['text']
