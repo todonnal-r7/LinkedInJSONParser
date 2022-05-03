d823d5a267c8bb0a2b146ec26cba3bbe76d3c008
todonnal-r7 <31863079+todonnal-r7@users.noreply.github.com>
2022-05-03 09:54:51 -0500

    Updates to match new JSON output format
    Modified parsing code to handle new JSON format on LinkedIn as of May 3, 2022

--- LinkedInJSONParser.py
+++ LinkedInJSONParser.py
@@ -10,17 +10,17 @@ inputFile = args.file
 with open(inputFile) as f:
 	jsonData = json.load(f)
 
-if (len(jsonData['data']['elements']) > 1):
-	for x in range(0,len(jsonData['data']['elements'][1]['elements'])):
-		fullName = jsonData['data']['elements'][1]['elements'][x]['title']['text']
+if (len(jsonData['included']) > 1):
+	for x in range(10,20):
+		fullName = jsonData['included'][x]['title']['text']
 		if (fullName == 'LinkedIn Member' or fullName == ''):
 			continue
 		else:
-			print(jsonData['data']['elements'][1]['elements'][x]['title']['text']+":"+jsonData['data']['elements'][1]['elements'][x]['headline']['text'])+":"+jsonData['data']['elements'][1]['elements'][x]['subline']['text']
+			print(jsonData['included'][x]['title']['text']+":"+jsonData['included'][x]['primarySubtitle']['text'])+":"+jsonData['included'][x]['secondarySubtitle']['text']
 else:
-	for x in range(0,len(jsonData['data']['elements'][0]['elements'])):
-		fullName = jsonData['data']['elements'][0]['elements'][x]['title']['text']
+	for x in range(10,len(jsonData['data']['elements'])):
+		fullName = jsonData['data']['elements'][x]['title']['text']
 		if (fullName == 'LinkedIn Member' or fullName == ''):
 			continue
 		else:
-			print(jsonData['data']['elements'][0]['elements'][x]['title']['text']+":"+jsonData['data']['elements'][0]['elements'][x]['headline']['text'])+":"+jsonData['data']['elements'][0]['elements'][x]['subline']['text']
+			print(jsonData['data']['elements'][x]['title']['text']+":"+jsonData['data']['elements'][x]['primarySubtitle']['text'])+":"+jsonData['data']['elements'][x]['secondarySubtitle']['text']
